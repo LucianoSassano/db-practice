b3ac1ef6e082e0b05c3986a65b9daa5b0488bb4a
Luciano_Sassano <lucianosassano1@gmail.com>
2020-03-03 11:31:46 -0300

    enviroment file added

--- controllers/task.js
+++ controllers/task.js
@@ -1,3 +1,11 @@
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user:"root",
+//   password:"",
+//   database:"bakery"
+// });
+
 exports.index = (req, res) => {
   tasks = [
     {
@@ -20,20 +28,26 @@ exports.show = (req, res) => {
   res.json({ tasks: tasks });
 };
 
-exports.store = (req, res) => {
-  const data = req.body;
-  console.log("body", data);
-  
-  tasks = {
-    name: "task ",
-    description: "description task"
-  };
-  res.json({ tasks: tasks });
-};
+// exports.store = (req, res) => {
+//   const data = req.body;
+//   console.log("body", data);
+//   var sql = `INSERT INTO tasks 
+//             (
+//                 Title, Description, isDone
+//             )
+//             VALUES
+//             (
+//                 ?, ?, ?
+//             )`;
+
+
+//   res.json({ tasks: tasks });
+// };
 
 exports.delete = (req,res) => {
   tasks = {
-    name: "task" +req.params.id,
+    name: " task " + req.params.id ,
     description: "task description"
-  }
+  };
+  res.json({tasks:tasks})
 }
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const routes = require( '../routes/routes' )
+const morgan = require('morgan')
+var bodyParser = require('body-parser')
+require('dotenv').config()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.use( morgan( "dev") );
+routes(app)
+console.log(process.env.DB_USER);
+app.get("*", (req, res) => res.status(400).send({
+	message: "No se encuentra el recurso"
+}));
+// app.use(function (req, res) {
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.write('you posted:\n')
+//     res.end(JSON.stringify(req.body, null, 2))
+//   })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
